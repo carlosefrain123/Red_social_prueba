8b26032a6dc5c86662ab355bdb07317274c6e3a3
Efrain <carlosefrainchallo1@gmail.com>
2024-06-24 19:49:26 -0500

    24/06/2024 - 07:48 - Se agrega la migración delted_at_to_users_table y se modifico el mdelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
-    use SoftDeletes;
+    use SoftDeletes,HasSlug;
 
     /**
      * The attributes that are mass assignable.
@@ -45,5 +48,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('username');
     }
 }
